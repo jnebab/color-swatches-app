ff03515bbc374fd3bfe8a1cc5c5034beb5d2f6d9
John Carl James Nebab <nebab.johncarl@gmail.com>
2020-07-28 05:12:06 +0800

    update update api url

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,5 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:4000/',
+    uri: 'https://unruffled-allen-f27ece.netlify.app/',
 });
